3312c1b354875726f86ad1e0d84775f3d183035c
timicodes21 <timileyinbabalola42@gmail.com>
2023-02-21 14:44:08 +0100

    Updates

--- src/components/pages/home/Congregation.tsx
+++ src/components/pages/home/Congregation.tsx
@@ -15,7 +15,7 @@ const Congregation = () => {
           </p>
 
           <Box sx={{ mt: 5 }}>
-            <Carousel>
+            <Carousel animation="slide">
               {congregation?.map((item, index) => (
                 <Box key={index}>
                   <ReactPlayer url={item?.url} width="100%" />
