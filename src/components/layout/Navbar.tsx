d3cd4508ea4bfb0b097536dd4f1736d47ec28661
Tripleoniks <84743221+Tripleoniks@users.noreply.github.com>
2023-04-07 14:46:04 +0100

    comment out language dropdown

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-import Wrapper from "./Wrapper";
-import Image from "next/image";
-import { navbarHomeItems, navbarItems, navbarWhereItems } from "@/data/navbar";
-import TransparentButton from "../buttons/TransparentButton";
-import BlueButton from "../buttons/BlueButton";
-import { useRouter } from "next/router";
-import { useNavbar } from "@/hooks/useNavbar";
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import { IoMdClose } from "react-icons/io";
-import classes from "../../styles/Home.module.css";
-import CustomModal from "../modals/CustomModal";
-import Link from "next/link";
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import Wrapper from './Wrapper';
+import Image from 'next/image';
+import { navbarHomeItems, navbarItems, navbarWhereItems } from '@/data/navbar';
+import TransparentButton from '../buttons/TransparentButton';
+import BlueButton from '../buttons/BlueButton';
+import { useRouter } from 'next/router';
+import { useNavbar } from '@/hooks/useNavbar';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
+import { IoMdClose } from 'react-icons/io';
+import classes from '../../styles/Home.module.css';
+import CustomModal from '../modals/CustomModal';
+import Link from 'next/link';
 
 const Navbar = () => {
   const router = useRouter();
@@ -30,52 +30,52 @@ const Navbar = () => {
   return (
     <Box
       sx={{
-        background: "#FFFEF9",
-        boxShadow: "0px 4px 38px rgba(0, 0, 0, 0.05)",
+        background: '#FFFEF9',
+        boxShadow: '0px 4px 38px rgba(0, 0, 0, 0.05)',
         py: { xs: 1, md: 1 },
-        position: "fixed",
-        width: "100%",
+        position: 'fixed',
+        width: '100%',
         zIndex: 1000,
       }}
-      className={show ? "nav_container inter" : "nav_container hide"}
+      className={show ? 'nav_container inter' : 'nav_container hide'}
     >
       <Wrapper>
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "space-between",
+            display: 'flex',
+            justifyContent: 'space-between',
           }}
         >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Box sx={{ display: { xs: "none", md: "block" } }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: { xs: 'none', md: 'block' } }}>
               <Image
                 src="/assets/icons/small_logo.svg"
                 width={62}
                 height={49}
                 alt="logo"
-                onClick={() => router.push("/home")}
+                onClick={() => router.push('/home')}
                 className="pointer"
               />
             </Box>
-            <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <Box sx={{ display: { xs: 'block', md: 'none' } }}>
               <Image
                 src="/assets/icons/small_logo.svg"
                 width={41}
                 height={32.5}
                 alt="logo"
-                onClick={() => router.push("/home")}
+                onClick={() => router.push('/home')}
                 className="pointer"
               />
             </Box>
 
-            <Box sx={{ display: { xs: "none", lg: "flex" } }}>
+            <Box sx={{ display: { xs: 'none', lg: 'flex' } }}>
               <Box sx={{ ml: 2 }} className="dropdown">
                 <Link
                   href="/home"
                   className={
-                    router.pathname === "/home"
-                      ? "navbar_link_active"
-                      : "navbar_link"
+                    router.pathname === '/home'
+                      ? 'navbar_link_active'
+                      : 'navbar_link'
                   }
                 >
                   Where we Started
@@ -87,8 +87,8 @@ const Navbar = () => {
                         href={item.link}
                         className={
                           router.pathname === item?.link
-                            ? "navbar_link_active"
-                            : "navbar_link"
+                            ? 'navbar_link_active'
+                            : 'navbar_link'
                         }
                       >
                         {item.text}
@@ -102,9 +102,9 @@ const Navbar = () => {
                   <Link
                     href="/where_we_are_going"
                     className={
-                      router.pathname.includes("/where_we_are_going")
-                        ? "navbar_link_active"
-                        : "navbar_link"
+                      router.pathname.includes('/where_we_are_going')
+                        ? 'navbar_link_active'
+                        : 'navbar_link'
                     }
                   >
                     Where we are going
@@ -116,8 +116,8 @@ const Navbar = () => {
                           href={item.link}
                           className={
                             router.pathname === item?.link
-                              ? "navbar_link_active"
-                              : "navbar_link"
+                              ? 'navbar_link_active'
+                              : 'navbar_link'
                           }
                         >
                           {item.text}
@@ -130,15 +130,15 @@ const Navbar = () => {
             </Box>
           </Box>
           <Box
-            sx={{ display: { xs: "none", lg: "flex" }, alignItems: "center" }}
+            sx={{ display: { xs: 'none', lg: 'flex' }, alignItems: 'center' }}
           >
-            <TransparentButton
+            {/* <TransparentButton
               onClick={toggleLanguageModal}
               dropdown
               size="small"
             >
               {languageText}
-            </TransparentButton>
+            </TransparentButton> */}
             <Box sx={{ mx: 1 }}></Box>
             <a href="#excited">
               <BlueButton onClick={() => {}} arrow size="small">
@@ -147,32 +147,32 @@ const Navbar = () => {
             </a>
           </Box>
           <Box
-            sx={{ display: { xs: "flex", lg: "none" }, alignItems: "center" }}
+            sx={{ display: { xs: 'flex', lg: 'none' }, alignItems: 'center' }}
           >
             <HiOutlineMenuAlt3
-              style={{ color: "#74757E", fontWeight: 900, fontSize: "28px" }}
+              style={{ color: '#74757E', fontWeight: 900, fontSize: '28px' }}
               onClick={mobileNav ? closeNav : toggleNav}
               className="pointer"
             />
           </Box>
           <Box
-            className={`mobile-navbar ${mobileNav && "open"}`}
-            sx={{ background: "#FFF", p: 2 }}
+            className={`mobile-navbar ${mobileNav && 'open'}`}
+            sx={{ background: '#FFF', p: 2 }}
           >
             <Box
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
-                background: "#FFF",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                background: '#FFF',
               }}
             >
               <Box>
                 <IoMdClose
                   style={{
-                    color: "#74757E",
+                    color: '#74757E',
                     fontWeight: 1000,
-                    fontSize: "28px",
+                    fontSize: '28px',
                   }}
                   onClick={closeNav}
                   className="pointer"
@@ -181,9 +181,9 @@ const Navbar = () => {
             </Box>
             <Box
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
                 mt: 2,
               }}
             >
@@ -191,9 +191,9 @@ const Navbar = () => {
                 <Link
                   href="/home"
                   className={
-                    router.pathname === "/home"
-                      ? "navbar_link_active"
-                      : "navbar_link"
+                    router.pathname === '/home'
+                      ? 'navbar_link_active'
+                      : 'navbar_link'
                   }
                   onClick={closeNav}
                 >
@@ -206,8 +206,8 @@ const Navbar = () => {
                         href={item.link}
                         className={
                           router.pathname === item?.link
-                            ? "navbar_link_active"
-                            : "navbar_link"
+                            ? 'navbar_link_active'
+                            : 'navbar_link'
                         }
                       >
                         {item.text}
@@ -220,9 +220,9 @@ const Navbar = () => {
                 <Link
                   href="/where_we_are_going"
                   className={
-                    router.pathname === "/where_we_are_going"
-                      ? "navbar_link_active"
-                      : "navbar_link"
+                    router.pathname === '/where_we_are_going'
+                      ? 'navbar_link_active'
+                      : 'navbar_link'
                   }
                   onClick={closeNav}
                 >
@@ -235,8 +235,8 @@ const Navbar = () => {
                         href={item.link}
                         className={
                           router.pathname === item?.link
-                            ? "navbar_link_active"
-                            : "navbar_link"
+                            ? 'navbar_link_active'
+                            : 'navbar_link'
                         }
                       >
                         {item.text}
@@ -246,7 +246,7 @@ const Navbar = () => {
                 </Box>
               </Box>
             </Box>
-            <Box sx={{ display: "flex", flexDirection: "column", mt: 3 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', mt: 3 }}>
               <TransparentButton onClick={toggleLanguageModal} dropdown>
                 {languageText}
               </TransparentButton>
@@ -265,7 +265,7 @@ const Navbar = () => {
         closeModal={toggleLanguageModal}
         hideCloseIcon
       >
-        {["English", "Yoruba"].map((item, index) => (
+        {['English', 'Yoruba'].map((item, index) => (
           <Box
             className="pointer"
             sx={{ py: 1 }}
